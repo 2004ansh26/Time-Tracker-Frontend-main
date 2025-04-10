2f3ff1d60423263a245239afe31100437f610c7f
Prajapati Ansh <prajapatiansh137@gmail.com>
2025-04-10 10:18:09 +0530

    Show charts on frontend

--- src/components/Admin/Reports.jsx
+++ src/components/Admin/Reports.jsx
@@ -56,6 +56,18 @@ const Reports = () => {
       <AdminNavbar />
       <div className="reports-container">
   {/* <h2 className="reports-title">Admin / Manager Reports</h2> */}
+      <button
+      onClick={() => window.open('http://localhost:8000/generate-report', '_parent')}
+      className="report-btn px-4 py-2 bg-green-600 text-white rounded-md"
+    >
+      Download Excel Report
+    </button>
+      <button
+      onClick={() => window.open('http://localhost:8000/report/task-time-report', '_parent')}
+      className="report-btn px-4 py-2 bg-green-600 text-white rounded-md"
+    >
+      Download Task Report
+    </button>
 
   <div className="reports-grid">
     {/* Time per Developer */}
